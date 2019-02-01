6ada9d39be081d4f3a402818f29fcb81c18ea825
Richard <Richard>
2019-02-01 18:42:46 +0000

    test ast

--- compiler.js
+++ compiler.js
@@ -2,15 +2,52 @@ let _memory = [];
 
 
 function string(i,len){
+  if(typeof i == "object"){
+    return JSON.stringify(i)
+  }
+  let s = "";
+  for(let j=i;j<i+len;j++){
+    s += String.fromCharCode(_memory[j]);
+  }
+  return s;
+}
 
+function append(ast,item){
+  ast.push(item);
+  return ast;
+}
+
+function dictionary(s){
+  JSON.stringify(s);
 }
 
 function malloc(len){
     return 0;
 }
 
+function parse(file){
+  ast = []
+  main_function = {}
+  main_function["type"] = "function"
+  main_function["name"] = "main"
+  main_function["return_type"] = "number"
+  body = []
+  return_42 = {}
+  append(body,return_42)
+  return_42["type"] = "return"
+  value_42 = {}
+  value_42["type"] = "number"
+  value_42["value"] = 42
+  return_42["value"] = value_42
+  main_function["body"] = body
+  append(ast,main_function)
+  return ast
+}
+
 function compile(i,len) {
-  let file = string(i,len)
+  let file = string(i,len);
+  let ast = parse(file);
+  console.log(string(ast));
   _memory[0] = 0;
   _memory[1] = 0;
   _memory[2] = 0;
