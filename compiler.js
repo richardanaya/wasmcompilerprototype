15323d585c30352b1086e1d3cfa585f9599dfc0a
Richard <Richard>
2019-02-01 20:54:53 +0000

    basic lexer

--- compiler.js
+++ compiler.js
@@ -47,7 +47,8 @@ function append(ast,item){
 }
 
 function dictionary(s){
-  JSON.stringify(s);
+  if(typeof s == "string") { return JSON.parse(s)};
+  return s;
 }
 
 function len(v){
@@ -55,7 +56,68 @@ function len(v){
 }
 
 // compiler code
-function parse(file){
+CHAR_SPACE = ' '
+CHAR_NEWLINE = '\n'
+CHAR_PAREN = '('
+CHAR_PAREN_END = ')'
+CHAR_CURLY_PAREN = '{'
+CHAR_CURLY_PAREN_END = '}'
+CHAR_BRACKET = '['
+CHAR_BRACKET_END = ']'
+CHAR_1 = '1'
+CHAR_2 = '2'
+CHAR_3 = '3'
+CHAR_4 = '4'
+CHAR_5 = '5'
+CHAR_6 = '6'
+CHAR_7 = '7'
+CHAR_8 = '8'
+CHAR_9 = '9'
+CHAR_0 = '0'
+
+function lex(file){
+  symbols = []
+  i = 0
+  l = len(file)
+  new_symbol = true
+  error = ""
+  while(true) {
+    if(i == l) { break }
+    c = file[i]
+    if(c == CHAR_SPACE || c == CHAR_NEWLINE){
+      if(new_symbol != true){
+        new_symbol = true;
+      }
+    } else if(c == CHAR_PAREN || c == CHAR_PAREN_END || c == CHAR_CURLY_PAREN || c == CHAR_CURLY_PAREN_END || c == CHAR_BRACKET || c == CHAR_BRACKET_END){
+      if(new_symbol == true){
+        append(symbols,{type:"symbol",value:c})
+      }
+    } else {
+      if(new_symbol == true){
+        append(symbols,{type:"identifier",value:""})
+        new_symbol = false;
+      }
+      sym = dictionary(symbols[len(symbols)-1])
+      if(sym.value == "" && c == CHAR_1 || c == CHAR_2 || c == CHAR_3 || c == CHAR_4 || c == CHAR_5 || c == CHAR_6 || c == CHAR_7 || c == CHAR_8 || c == CHAR_9 || c == CHAR_0 ){
+        sym.type = "number"
+      }
+      if(sym.type == "number"){
+        if(c == CHAR_1 || c == CHAR_2 || c == CHAR_3 || c == CHAR_4 || c == CHAR_5 || c == CHAR_6 || c == CHAR_7 || c == CHAR_8 || c == CHAR_9 || c == CHAR_0 ){
+          sym.value += c
+        } else {
+          error = "error invalid number:" + sym.value + c
+          break
+        }
+      } else {
+          sym.value += c
+      }
+    }
+    i++;
+  }
+  return {symbols:symbols,error:error}
+}
+
+function parse(symbols){
   ast = []
   main_function = {
     type: "function",
@@ -87,7 +149,14 @@ function file(len){
 
 function compile(code_offset,code_len) {
   file = string(code_offset,code_len)
-  ast = parse(file)
+  lex_result = lex(file)
+  if(! is_empty(lex_result.error)){
+    console.log(lex_result.error)
+    return
+  }
+  symbols = lex_result.symbols
+  console.log(symbols)
+  ast = parse(symbols)
   wasm = compile_ast(ast)
   console.log(string(ast))
   wasmResponse = mem_alloc(1+len(wasm))
