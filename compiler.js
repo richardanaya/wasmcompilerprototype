c294180d2bb93614d9b942749b2a176fa5a87ad8
Richard <Richard>
2019-02-01 21:55:18 +0000

    basic parser for simple example

--- compiler.js
+++ compiler.js
@@ -56,24 +56,24 @@ function len(v){
 }
 
 // compiler code
-CHAR_SPACE = ' '
-CHAR_NEWLINE = '\n'
-CHAR_PAREN = '('
-CHAR_PAREN_END = ')'
-CHAR_CURLY_PAREN = '{'
-CHAR_CURLY_PAREN_END = '}'
-CHAR_BRACKET = '['
-CHAR_BRACKET_END = ']'
-CHAR_1 = '1'
-CHAR_2 = '2'
-CHAR_3 = '3'
-CHAR_4 = '4'
-CHAR_5 = '5'
-CHAR_6 = '6'
-CHAR_7 = '7'
-CHAR_8 = '8'
-CHAR_9 = '9'
-CHAR_0 = '0'
+CHAR_SPACE = " "
+CHAR_NEWLINE = "\n"
+CHAR_PAREN = "("
+CHAR_PAREN_END = ")"
+CHAR_CURLY_PAREN = "{"
+CHAR_CURLY_PAREN_END = "}"
+CHAR_BRACKET = "["
+CHAR_BRACKET_END = "]"
+CHAR_1 = "1"
+CHAR_2 = "2"
+CHAR_3 = "3"
+CHAR_4 = "4"
+CHAR_5 = "5"
+CHAR_6 = "6"
+CHAR_7 = "7"
+CHAR_8 = "8"
+CHAR_9 = "9"
+CHAR_0 = "0"
 
 function lex(file){
   symbols = []
@@ -89,9 +89,8 @@ function lex(file){
         new_symbol = true;
       }
     } else if(c == CHAR_PAREN || c == CHAR_PAREN_END || c == CHAR_CURLY_PAREN || c == CHAR_CURLY_PAREN_END || c == CHAR_BRACKET || c == CHAR_BRACKET_END){
-      if(new_symbol == true){
-        append(symbols,{type:"symbol",value:c})
-      }
+      append(symbols,{type:"symbol",value:c})
+      new_symbol = true
     } else {
       if(new_symbol == true){
         append(symbols,{type:"identifier",value:""})
@@ -117,9 +116,123 @@ function lex(file){
   return {symbols:symbols,error:error}
 }
 
+function parse_return(symbols,i){
+  let statement = {type:"return"}
+  error = ""
+
+  while(true){
+    sym = symbols[i]
+    if(i == len(symbols)){
+      error = "unexpected end of file"
+      break
+    } else if(sym.type =="number"){
+      statement.value = {type:"number",value:sym.value}
+      break
+    }
+    i+=1
+  }
+
+  return {statement:statement,error:error,next:i}
+}
+
+function parse_statements(symbols,i){
+  let statements = []
+  error = ""
+
+  while(true){
+    sym = symbols[i]
+    params_started = 0
+    if(i == len(symbols)){
+      error = "unexpected end of file"
+      break
+    } else if(sym.type =="identifier" && sym.value == "return"){
+      parse_result = parse_return(symbols,i)
+      if(! is_empty(parse_result.error)){
+        error = parse_result.error
+        break
+      }
+      append(statements,parse_result.statement)
+      i = parse_result.next+1
+      break
+    } else if(sym.type =="symbol" && sym.value == "}"){
+      break
+    }
+    i+=1
+  }
+
+  return {statements:statements,error:error,next:i}
+}
+
+function parse_function(symbols,i){
+  let ast = {type:"function", name:"",imported:0,exported:0,statements:[]}
+  error = ""
+  params_started = 0
+  while(true){
+    sym = symbols[i]
+    if(i == len(symbols)){
+      error = "unexpected end of file"
+      break
+    } else if(sym.type =="identifier" && sym.value == "export"){
+      ast.exported = 1
+    } else if(is_empty(ast.name) && sym.type =="identifier"){
+      ast.name = sym.value
+    } else if(!is_empty(ast.name) && params_started == 0 && sym.type =="symbol" && sym.value == "("){
+      //do nothing
+      params_started = 1
+    } else if(!is_empty(ast.name) && params_started == 1 && sym.type =="symbol" && sym.value == ")"){
+      if(i+1==len(symbols)){
+        ast.imported = 1
+      } else {
+          next_sym = dictionary(symbols[i+1])
+          if(next_sym.type == "symbol" || next_sym.value == "{"){
+            parse_result = parse_statements(symbols,i)
+            if(! is_empty(parse_result.error)){
+              error = parse_result.error
+              break
+            }
+            ast.statements = parse_result.statements
+            i = parse_result.next
+            break
+          } else {
+            error = "unexpected start to function body"
+            break
+          }
+      }
+    } else {
+      error = "unknown identifier in function def: "+string(sym)
+      break
+    }
+    i+=1
+  }
+
+  return {function:ast,error:error,next:i}
+}
+
 function parse(symbols){
-  ast = []
-  main_function = {
+  let ast = []
+  error = ""
+  i = 0
+  while(true){
+    if(i==len(symbols)){break}
+    sym = symbols[i]
+    if(sym.type =="identifier"){
+      if(sym.value == "export") {
+        parse_result = parse_function(symbols,i)
+        if(! is_empty(parse_result.error)){
+          error = parse_result.error
+          break
+        }
+        console.log(ast)
+        append(ast,parse_result.function)
+        i = parse_result.next
+      }
+    } else {
+      error = "unknown identifier: "+string(sym)
+      break
+    }
+    i+=1;
+  }
+  /*main_function = {
     type: "function",
     name: "main",
     exported: 1,
@@ -134,8 +247,8 @@ function parse(symbols){
         }
     ]
   }
-  append(ast,main_function)
-  return ast
+  append(ast,main_function)*/
+  return {ast:ast,error:error}
 }
 
 function compile_ast(ast){
@@ -156,7 +269,12 @@ function compile(code_offset,code_len) {
   }
   symbols = lex_result.symbols
   console.log(symbols)
-  ast = parse(symbols)
+  parse_result = parse(symbols)
+  if(! is_empty(parse_result.error)){
+    console.log(parse_result.error)
+    return
+  }
+  ast = parse_result.ast
   wasm = compile_ast(ast)
   console.log(string(ast))
   wasmResponse = mem_alloc(1+len(wasm))
@@ -173,8 +291,8 @@ function compile(code_offset,code_len) {
 }
 
 //client example
-var fs = require('fs');
-var contents = fs.readFileSync(process.argv[2], 'utf8');
+var fs = require("fs");
+var contents = fs.readFileSync(process.argv[2], "utf8");
 let start = file(contents.length);
 for(let i=0;i<contents.length;i++){
   _memory[i] = contents.charCodeAt(i)
